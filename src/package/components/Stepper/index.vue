bc4154e8e83f15939d031256a06af59e2926a8cb
HangMine <95629358@qq.com>
2021-05-06 23:28:49 +0800

    stepper

--- src/package/components/Stepper/index.vue
+++ src/package/components/Stepper/index.vue
@@ -5,15 +5,90 @@ export default defineComponent({
   props: {},
   setup(props, { attrs, emit, slots }) {
     const state = reactive({});
-    return { ...toRefs(state) };
+    const leftBtn = () => {
+      return (
+        <span class="r-stepper__iconbox r-stepper__icon--des">
+          <r-icon
+            name="iconjian"
+            color=""
+            size={20}
+            class="r-stepper__icon"
+          ></r-icon>
+        </span>
+      );
+    };
+    const rightBtn = () => {
+      return (
+        <span class="r-stepper__iconbox r-stepper__icon--inc">
+          <r-icon
+            name="iconjia"
+            color=""
+            size={20}
+            class="r-stepper__icon"
+          ></r-icon>
+        </span>
+      );
+    };
+
+    return { ...toRefs(state), leftBtn, rightBtn };
   },
   render() {
-    return <div class="r-stepper">步进器</div>;
+    const { leftBtn, rightBtn } = this;
+    return (
+      <div class="r-stepper">
+        {leftBtn()}
+        <input class="r-stepper__input" inputmode="decimal" />
+        {rightBtn()}
+      </div>
+    );
   },
 });
 </script>
 <style lang="scss" scpoed>
 /* @import url() */
 .r-stepper {
+  &__input {
+    box-sizing: border-box;
+    width: 32px;
+    height: 28px;
+    margin: 0 2px;
+    padding: 0;
+    color: #323233;
+    font-size: 14px;
+    line-height: normal;
+    text-align: center;
+    vertical-align: middle;
+    background-color: #f2f3f5;
+    border: 0;
+    border-width: 1px 0;
+    border-radius: 0;
+    -webkit-appearance: none;
+  }
+  &__iconbox {
+    display: inline-block;
+    width: 32px;
+    height: 28px;
+    vertical-align: middle;
+    line-height: 1.75rem;
+    text-align: center;
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    color: #323233;
+    background-color: #f2f3f5;
+    border: 0;
+    cursor: pointer;
+  }
+  &__icon {
+    vertical-align: middle;
+  }
+  &__icon--des {
+    border-top-left-radius: 4px;
+    border-bottom-left-radius: 4px;
+  }
+  &__icon--inc {
+    border-top-right-radius: 4px;
+    border-bottom-right-radius: 4px;
+  }
 }
 </style>
